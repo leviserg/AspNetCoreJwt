9a2b879f24e9b31b8e48fd51bdb8a735d44c74d4
leviserg <sergey.levitskiy.sl@gmail.com>
2022-01-09 17:52:49 +0200

    Add project files.

---
+++ AspNetCoreJwt/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetCoreJwt
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // token issuer
+        public const string AUDIENCE = "AspNetCoreJwt"; // token consumer - your application
+        const string KEY = "mysupersecret_secretkey!123";   // encruption key
+        public const int LIFETIME = 1; // token lifecycle
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
---
+++ AspNetCoreJwt/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreJwt.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [Authorize]
+        [Route("getlogin")]
+        public IActionResult GetLogin()
+        {
+            return Ok($"Your login: {User.Identity.Name}");
+        }
+
+        [Authorize(Roles = "admin")]
+        [Route("getrole")]
+        public IActionResult GetRole()
+        {
+            return Ok("Your ROLE: Administrator");
+        }
+    }
+}
